61b255b4354ddc002cfb96d52ee578be65cb1190
justin <144488087+yaoolee@users.noreply.github.com>
2025-08-06 22:35:05 -0400

    change gitignore

--- test-https.js
+++
@@ -1,8 +0,0 @@
-// test-https.js
-import https from 'https';
-
-https.get('https://pokeapi.co/api/v2/type/electric', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log(data));
-}).on('error', err => console.error(err));
